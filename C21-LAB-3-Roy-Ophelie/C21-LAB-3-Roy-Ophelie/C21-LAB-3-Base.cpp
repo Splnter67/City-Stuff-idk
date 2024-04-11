c8a460b631c236eb566075ec40078422defbfb1e
Splnter67 <77417062+Splnter67@users.noreply.github.com>
2024-04-11 15:06:28 -0400

    Added up to stat 10, next we must finish stat10

--- C21-LAB-3-Roy-Ophelie/C21-LAB-3-Roy-Ophelie/C21-LAB-3-Base.cpp
+++ C21-LAB-3-Roy-Ophelie/C21-LAB-3-Roy-Ophelie/C21-LAB-3-Base.cpp
@@ -52,12 +52,12 @@ int stat01(bool r1[]);
 int stat02(Protection* pro);
 double stat03(Protection* pro, Repondant rep[]);
 double stat04(Protection* pro, Repondant rep[]);
-double stat05(Protection* pro);
-double stat06(Protection* pro);
-string stat07(Infection inf[]);
-int stat08(Infection inf[]);
-double stat09(Infection inf[]);
-string stat10(Infection inf[]);
+double stat05(Protection* pro, Repondant rep[]);
+double stat06(Infection* inf);
+string stat07(Infection* inf);
+int stat08(Protection* pro, Infection* inf);
+double stat09(Protection* pro, Infection* inf, Repondant rep[]);
+string stat10(Infection* inf, Repondant rep[]);
 
 
 
@@ -144,33 +144,166 @@ double stat04(Protection* pro, Repondant rep[])
 	return mm;
 }
 
-double stat05(Protection* pro)
+double stat05(Protection* pro, Repondant rep[])
 {
-	return 0;
+	int r;
+	double nv = 0, nm = 0, v = 0;
+	for (r = 0; r <= nb_repondants; r++) {
+		if (rep[r].ville == "Vaudreuil") {
+			v++;
+			if (pro->r2[r] > 1) {
+				nv++;
+			}
+		}
+	}
+	nm = (nv / v) * 100;
+	return nm;
 }
 
-double stat06(Protection* pro)
+double stat06(Infection* inf)
 {
-	return 0;
+	int r;
+	double lp = 0, l = 0;
+	for (r = 0; r <= nb_repondants; r++) {
+		if (inf[r].r7 == true) {
+			l++;
+		}
+	}
+	lp = (l / nb_repondants) * 100;
+	return lp;
 }
 
-string stat07(Infection inf[])
+string stat07(Infection* inf)
 {
-	return "false";
+	int r, n = 0, sv = 0, p = 0, ej = 0, af = 0;
+	string sel;
+	for (r = 0; r <= nb_repondants; r++) {
+		switch (inf[r].r8) {
+		case 1:
+			n++;
+			break;
+		case 2:
+			sv++;
+			break;
+		case 3:
+			p++;
+			break;
+		case 4:
+			ej++;
+			break;
+		case 5:
+			af++;
+			break;
+		default:
+			break;
+		}
+	}
+	if (n >= sv && n >= p && n >= ej && n >= af) {
+		sel = "Pas de désinfectant";
+	}
+	else if (sv >= n && sv >= p && sv >= ej && sv >= af) {
+		sel = "Savon à vaisselle";
+	}
+	else if (p >= n && p >= sv && p >= ej && p >= af) {
+		sel = "Purell";
+	}
+	else if (ej >= n && ej >= sv && ej >= p && ej >= af) {
+		sel = "Eau de javel";
+	}
+	else if (af >= n && af >= sv && af >= p && af >= ej) {
+		sel = "Alcool a friction";
+	}
+	else
+		"NA";
+
+	return sel;
 }
 
-int stat08(Infection inf[])
+int stat08(Protection* pro, Infection* inf)
 {
-	return 0;
+	int r, pnt = 0;
+	for (r = 0; r <= nb_repondants; r++) {
+		if (inf[r].r7 == 'N' && inf[r].r10 == 0 && pro->r4[r] == 'O') {
+			pnt++;
+		}
+	}
+	return pnt;
 }
 
-double stat09(Infection inf[])
+double stat09(Protection* pro, Infection* inf, Repondant rep[])
 {
-	return 0;
+	int r, d = 0;
+	double full = 0, m = 0;
+	for (r = 0; r <= nb_repondants; r++) {
+		if (pro->r2[r] == 0 && inf[r].r8 == 1 && inf[r].r9 == 1) {
+			d += rep[r].scolarite;
+			m++;
+		}
+	}
+	full = d / m;
+	return full;
 }
 
-string stat10(Infection inf[])
+string stat10(Infection* inf, Repondant rep[])
 {
+	int r, lg = 0, mtl = 0, lc = 0, vd = 0, bs = 0, bc = 0, tb = 0, aj = 0, lv = 0;
+	for (r = 0; r <= nb_repondants; r++) {
+		if (rep[r].ville == "Longueuil") {
+			lg += inf[r].r10;
+		}
+		else if (rep[r].ville == "Montréal") {
+			mtl += inf[r].r10;
+		}
+		else if (rep[r].ville == "Lachine") {
+			lc += inf[r].r10;
+		}
+		else if (rep[r].ville == "Vaudreuil") {
+			vd += inf[r].r10;
+		}
+		else if (rep[r].ville == "Brossard") {
+			bs += inf[r].r10;
+		}
+		else if (rep[r].ville == "Boucherville") {
+			bc += inf[r].r10;
+		}
+		else if (rep[r].ville == "Terrebonne") {
+			tb += inf[r].r10;
+		}
+		else if (rep[r].ville == "Anjou") {
+			aj += inf[r].r10;
+		}
+		else if (rep[r].ville == "Laval") {
+			lv += inf[r].r10;
+		}
+	}
+	if (lg >= mtl && lg >= lc && lg >= vd && lg >= bs && lg >= bc && lg >= tb && lg >= aj && lg >= lv) {
+
+	}
+	else if (mtl >= lg && mtl >= lc && mtl >= vd && mtl >= bs && mtl >= bc && mtl >= tb && mtl >= aj && mtl >= lv) {
+
+	}
+	else if (lc >= lg && lc >= mtl && lc >= vd && lc >= bs && lc >= bc && lc >= tb && lc >= aj && lc >= lv) {
+
+	}
+	else if (vd >= lg && vd >= mtl && vd >= lc && vd >= bs && vd >= bc && vd >= tb && vd >= aj && vd >= lv) {
+
+	}
+	else if (bs >= lg && bs >= mtl && bs >= lc && bs >= vd && bs >= bc && bs >= tb && bs >= aj && bs >= lv) {
+
+	}
+	else if (bc >= lg && bc >= mtl && bc >= lc && bc >= vd && bc >= bs && bc >= tb && bc >= aj && bc >= lv) {
+
+	}
+	else if (tb >= lg && tb >= mtl && tb >= lc && tb >= vd && tb >= bs && tb >= bc && tb >= aj && tb >= lv) {
+
+	}
+	else if (aj >= lg && aj >= mtl && aj >= lc && aj >= vd && aj >= bs && aj >= bc && aj >= tb && aj >= lv) {
+
+	}
+	else if (lv >= lg && lv >= mtl && lv >= lc && lv >= vd && lv >= bs && lv >= bc && lv >= tb && lv >= aj) {
+
+	}
+
 	return "o";
 }
 
@@ -196,7 +329,8 @@ int main()
 
 	// B - appeler vos fonctions statistiques et afficher ici leurs résultats
 	// ...
-	cout << fixed << setprecision(1) << stat01(r1) << endl << stat02(pro) << endl << stat03(pro, rep) << endl << stat04(pro, rep);
+	cout << fixed << setprecision(1) << stat01(r1) << endl << stat02(pro) << endl << stat03(pro, rep) << endl << stat04(pro, rep) << endl << stat05(pro, rep) << endl
+		<< stat06(inf) << endl << stat07(inf) << endl << stat08(pro, inf) << endl << stat09(pro, inf, rep);
 	
 
 
